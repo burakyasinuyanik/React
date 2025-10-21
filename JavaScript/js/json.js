398472df144c6153b1044c4bdcc262418413c8a9
burakyasinuyanik <116893884+burakyasinuyanik@users.noreply.github.com>
2025-10-22 00:07:15 +0300

    update

---
+++ JavaScript/js/json.js
@@ -0,0 +1,21 @@
+const info = `
+{
+  "user": "username",
+  "info": 1234234,
+  "arr": [{"i": 1, "k": 2}],
+  "isActive": true
+}
+`;
+
+console.log(typeof info);
+console.log(info);
+
+const parsedInfo = JSON.parse(info);
+
+console.log(typeof parsedInfo);
+console.log(parsedInfo);
+
+const stringifyInfo = JSON.stringify(info);
+
+console.log(typeof stringifyInfo);
+console.log(stringifyInfo);
