d11bb76370f08bba6e99bd1bb4805609cd62145b
burakyasinuyanik <116893884+burakyasinuyanik@users.noreply.github.com>
2025-10-27 23:59:37 +0300

    v2 create

---
+++ React/ReactPicoV2/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <main className='container'>
+        
+      </main>
+    </>
+  )
+}
+
+export default App
