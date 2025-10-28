7c7bad9e8adfda0e67323bd6e972827f99dca6b8
burakyasinuyanik <116893884+burakyasinuyanik@users.noreply.github.com>
2025-10-28 17:51:45 +0300

    form project start

---
+++ React/ReactForm/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='container'>
+        <div className='row justify-content-sm-center'>
+          <div className="col-sm-6 bg-primary">
+              
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
